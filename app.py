143808317d9003663040e47e4618d7b6d4ee33c2
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-08-19 00:13:38 +0300

    Added configuration

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/addNewBook')
+def addNewBook():
+    return 'Add New Book' 
+
 
 if __name__ == '__main__':
     app.run()
+
