7f9f556839c1211c7bcbadf525e2e5b8fc399203
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-08-19 15:14:49 +0300

    finished backend and integrating frontend

--- app.py
+++ app.py
@@ -4,52 +4,112 @@ import os
 
 app = Flask(__name__)
 
-# Load the inventory from the JSON file
-def load_inventory():
-    if os.path.exists('library.json'):
+
+
+def getInventory():
+    try:
         with open('library.json', 'r') as file:
-            return json.load(file) # Return the inventory if the file exists
-    return {"books": []} # Return an empty inventory if the file does not exist
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        return {"books":[] }
+    
+def saveInventory(Inventory):
+    try:
+        with open('library.json' , 'w') as file:
+            json.dump(Inventory , file)
+    except FileNotFoundError:
+        return FileNotFoundError
 
-# Save the inventory to the JSON file
-def save_inventory(inventory): 
-    with open('library.json', 'w') as file: # Open the file in write mode
-        json.dump(inventory, file, indent=4) # Write the inventory to the file using json.dump
-
-# Add a book to the inventory
-def add_book(title, author, isbn):
-    inventory = load_inventory() # Load the inventory
+def addBook(title , author , isbn):
+    inventory = getInventory()
     inventory['books'].append({
-        "title": title,
-        "author": author,
-        "isbn": isbn   #ISBN is the unique identifier for the book
+        "title" : title,
+        "author" : author,
+        "isbn" : isbn,
+        "status" : "Available"
     })
-    save_inventory(inventory)
+    saveInventory(inventory)
 
-# Remove a book from the inventory
-def remove_book(isbn):
-    inventory = load_inventory()
+def removeBook(isbn):
+    inventory = getInventory()
     inventory['books'] = [book for book in inventory['books'] if book['isbn'] != isbn]
-    save_inventory(inventory)
+    saveInventory(inventory)
 
+def getBooks(keyword):
+    inventory = getInventory()
+    result = []
+    for book in inventory:
+        if keyword.lower() in book['title'].lower() or keyword.lower() in book['author'].lower() or keyword == book['isbn']:
+            result.append(book)
+    return result
+
+def getBook(isbn):
+    inventory = getInventory()
+    for book in inventory['books']:
+        if book['isbn'] == isbn:
+            return book
+        
+def borrowBook(isbn):
+    inventory = getInventory()
+    for book in inventory['books']:
+        if book['isbn'] == isbn and book['status'] == 'Available':
+            book['status'] = 'Borrowed'
+            saveInventory(inventory)
+            return True
+    return False
+
+def returnBorrowedBook(isbn):
+    inventory = getInventory()
+    for book in inventory['books']:
+        if book['isbn'] == isbn:
+            book['status'] = 'Available'
+            saveInventory(inventory)
+            return True
+    return False
 
 @app.route('/')
 def index():
-    inventory = load_inventory()
-    return render_template('index.html', books=inventory['books'])
+    books = getInventory()
+    return render_template('index.html', books=books['books'])
 
 @app.route('/add_book', methods=['POST'])
-def handle_add_book():
+def add_book():
     title = request.form['title']
     author = request.form['author']
     isbn = request.form['isbn']
-    add_book(title, author, isbn)
+    addBook(title , author , isbn)
     return redirect(url_for('index'))
 
-@app.route('/remove_book/<isbn>', methods=['GET'])
-def handle_remove_book(isbn):
-    remove_book(isbn)
+@app.route('/remove_book/<isbn>') # Request parameter : isbn
+def remove_book(isbn):
+    removeBook(isbn)
     return redirect(url_for('index'))
+
+@app.route('/searchBooks') # Request parameter : keyword
+def search():
+    keyword = request.form['keyword']
+    books = getBooks(keyword)
+    return jsonify(books)
+
+@app.route('search') # Request parameter : isbn
+def searchBook():
+    isbn = request.form['isbn']
+    book = getBook(isbn)
+    return jsonify(book)
+
+
+@app.route('/borrow_book/<isbn>') # Request parameter : isbn
+def borrow_book(isbn):
+    if borrowBook(isbn):
+        return "Borrowed"
+    return "Not Available"
+
+@app.route('/return_book/<isbn>') # Request parameter : isbn
+def return_book(isbn):
+    if returnBorrowedBook(isbn):
+        return "Returned"
+    return "Not Borrowed"
 
 if __name__ == '__main__':
     app.run(debug=True)
