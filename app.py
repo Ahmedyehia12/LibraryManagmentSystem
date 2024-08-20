8cf80171a2c8ac468d8458979896c7e138e3d301
Marwan Mohammed Sayed <155132957+MarwanMohammed2500@users.noreply.github.com>
2024-08-20 10:16:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,117 +1,230 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 import json
-import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-
-def getInventory():
-    try:
-        with open('library.json', 'r') as file:  # Open the file in read mode
-            data = json.load(file) # Load the json data from the file
-            return data # Return the data
-    except FileNotFoundError: # If the file is not found
-        return {"books":[] } # Return an empty dictionary
+class library: # Where all common functions between users and admins are stored
+    def __init__(self):
+        pass
     
-def saveInventory(Inventory):
-    try:
-        with open('library.json' , 'w') as file: # Open the file in write mode
-            json.dump(Inventory , file) # Write the data to the file
-    except FileNotFoundError: # If the file is not found
-        return FileNotFoundError # Return the error
-
-def addBook(title , author , isbn): # Function to add a book to the inventory
-    inventory = getInventory() # Get the inventory
-    inventory['books'].append({ # Append the new book to the inventory
-        "title" : title, # Title of the book
-        "author" : author,  
-        "isbn" : isbn, 
-        "status" : "Available"
-    })
-    saveInventory(inventory) # Save the inventory
-
-def removeBook(isbn): # Function to remove a book from the inventory
-    inventory = getInventory() # Get the inventory
-    inventory['books'] = [book for book in inventory['books'] if book['isbn'] != isbn] # Remove the book with the given isbn
-    saveInventory(inventory) # Save the inventory
-
-def getBooks(keyword):
-    inventory = getInventory() # Get the inventory
-    result = [] # Create an empty list to store the search results
-    for book in inventory:  # Iterate through the books in the inventory
-        if keyword.lower() in book['title'].lower() or keyword.lower() in book['author'].lower() or keyword == book['isbn']:
-            result.append(book) # If the keyword is found in the title, author or isbn, add the book to the result list
-    return result
-
-def getBook(isbn):
-    inventory = getInventory() # Get the inventory
-    for book in inventory['books']: # Iterate through the books in the inventory
-        if book['isbn'] == isbn: # If the isbn matches
-            return book # Return the book
+    def getInventory(self):
+        try:
+            with open('library.json', 'r') as file:  # Open the file in read mode
+                data = json.load(file) # Load the json data from the file
+                return data # Return the data
+        except FileNotFoundError: # If the file is not found
+            return "File Not Found!" # Return an empty dictionary
+    
+    def getUsers(self):
+        try:
+            with open('users.json', 'r') as file:  # Open the file in read mode
+                data = json.load(file) # Load the json data from the file
+                return data # Return the data
+        except FileNotFoundError: # If the file is not found
+            return "File Not Found!" # Return an empty dictionary
         
-def borrowBook(isbn): # Function to borrow a book
-    inventory = getInventory() # Get the inventory
-    for book in inventory['books']: # Iterate through the books in the inventory
-        if book['isbn'] == isbn and book['status'] == 'Available': # If the isbn matches and the book is available
-            book['status'] = 'Borrowed' # Change the status to borrowed
-            saveInventory(inventory) # Save the inventory
-            return True # Return True
-    return False # Return False
 
-def returnBorrowedBook(isbn):
-    inventory = getInventory() # Get the inventory
-    for book in inventory['books']: # Iterate through the books in the inventory
-        if book['isbn'] == isbn: # If the isbn matches
-            book['status'] = 'Available' # Change the status to available
-            saveInventory(inventory) # Save the inventory
-            return True # Return True
-    return False
+    def saveUser(self, User):
+        try:
+            with open('users.json', 'w') as file: # Open the file in write mode
+                json.dump(User, file) # Write the data to the file
+        except FileNotFoundError: # If the file is not found
+            return FileNotFoundError # Return the error
+        
 
-@app.route('/') # Home route
+    def getBooks(self,keyword):
+        inventory = self.getInventory() # Get the inventory
+        result = [] # Create an empty list to store the search results
+        for book in inventory:  # Iterate through the books in the inventory
+            if keyword.lower() in book['title'].lower() or keyword.lower() in book['author'].lower() or keyword == book['isbn']:
+                result.append(book) # If the keyword is found in the title, author or isbn, add the book to the result list
+        return result
+
+    def getBook(self,isbn):
+        inventory = self.getInventory() # Get the inventory
+        for book in inventory['books']: # Iterate through the books in the inventory
+            if book['isbn'] == isbn: # If the isbn matches
+                return book # Return the book@app.route('/') # Home route
+    def index():
+        books = library().getInventory()
+        return render_template('index.html', books=books['books']) # Render the index.html template with the books
+    
+    @app.route('/searchBooks') # Request parameter : keyword
+    @login_required
+    def search():
+        keyword = request.form['keyword'] # Get the keyword from the form
+        books = library().getBooks(keyword) # Get the books that match the keyword
+        return jsonify(books) # Return the books as a json response
+    
+    @app.route('/search') # Request parameter : isbn
+    @login_required
+    def searchBook():
+        isbn = request.form['isbn'] # Get the isbn from the form
+        book = library().getBook(isbn) # Get the book with the given isbn
+        return jsonify(book) # Return the book as a json response
+
+
+
+class Admin_User(UserMixin, library): # Admin Privileges
+    def __init__(self, username):
+        self.id = username
+
+    @login_required
+    def addBook(self, title , author , isbn, genre, date): # Function to add a book to the inventory
+        inventory = library().getInventory() # Get the inventory
+        inventory['books'].append({ # Append the new book to the inventory
+            "title" : title, # Title of the book
+            "author" : author,
+            "isbn" : isbn,
+            "genre": genre,
+            "date_of_publish": date,
+            "status" : "Available"
+        })
+        self.saveInventory(inventory) # Save the inventory
+    
+    def saveInventory(self, Inventory):
+        try:
+            with open('library.json' , 'w') as file: # Open the file in write mode
+                json.dump(Inventory ,    file) # Write the data to the file
+        except FileNotFoundError: # If the file is not found
+            return FileNotFoundError # Return the error
+    
+    @login_required
+    def removeBook(self, isbn): # Function to remove a book from the inventory
+        inventory = library().getInventory() # Get the inventory
+        inventory['books'] = [book for book in inventory['books'] if book['isbn'] != isbn] # Remove the book with the given isbn
+        self.saveInventory(inventory) # Save the inventory  
+    
+        
+
+class User(UserMixin, library): # Normal User Privileges
+    def __init__(self, username):
+        self.id = username  
+
+    @login_manager.user_loader
+    def load_user(username):
+        if username in users_db.keys():
+            user = users_db[username]
+            return User(user)
+        return None
+
+    @login_required
+    def borrowBook(self, isbn): # Function to borrow a book
+        inventory = library().getInventory() # Get the inventory
+        for book in inventory['books']: # Iterate through the books in the inventory
+            if book['isbn'] == isbn and book['status'] == 'Available': # If the isbn matches and the book is available
+                book['status'] = 'Borrowed' # Change the status to borrowed
+                Admin_User(None).saveInventory(inventory) # Save the inventory
+                return True # Return True
+        return False # Return False
+    
+    @login_required
+    def returnBorrowedBook(isbn):
+        inventory = library().getInventory() # Get the inventory
+        for book in inventory['books']: # Iterate through the books in the inventory
+            if book['isbn'] == isbn: # If the isbn matches
+                book['status'] = 'Available' # Change the status to available
+                Admin_User(None).saveInventory(inventory) # Save the inventory
+                return True # Return True
+        return False
+
+
+@app.route('/admin_home') # Home route
+@login_required
 def index():
-    books = getInventory()
+    books = library().getInventory()
     return render_template('index.html', books=books['books']) # Render the index.html template with the books
+
+
+
+@app.route('/home')
+@login_required
+def index_user():
+    books = library().getInventory()
+    return render_template('index_user.html', books=books['books']) # Render the index.html template with the books
 
 @app.route('/add_book', methods=['POST']) # Request method : POST
 def add_book(): # Function to add a book
-    if getBook(request.form['isbn']): # If the book already exists
+    if Admin_User(None).getBook(request.form['isbn']): # If the book already exists
         return redirect(url_for('index')) # Redirect to the index page
     title = request.form['title'] # Get the title from the form
     author = request.form['author'] # Get the author from the form
     isbn = request.form['isbn'] # Get the isbn from the form
-    addBook(title , author , isbn) # Add the book to the inventory
+    genre = request.form['genre'] # Get the genre of the book
+    date = request.form['date_of_publish'] # get the date of publish
+    Admin_User(None).addBook(title , author , isbn, genre, date) # Add the book to the inventory
     return redirect(url_for('index')) # Redirect to the index page
 
-@app.route('/remove_book/<isbn>') # Request parameter : isbn
+@app.route('/remove_book/<isbn>', methods=["GET", "POST"]) # Request parameter : isbn
 def remove_book(isbn): # Function to remove a book
-    removeBook(isbn) # Remove the book from the inventory
-    return redirect(url_for('index')) # Redirect to the index page  
-
-@app.route('/searchBooks') # Request parameter : keyword
-def search():
-    keyword = request.form['keyword'] # Get the keyword from the form
-    books = getBooks(keyword) # Get the books that match the keyword
-    return jsonify(books) # Return the books as a json response
-
-@app.route('/search') # Request parameter : isbn
-def searchBook():
-    isbn = request.form['isbn'] # Get the isbn from the form
-    book = getBook(isbn) # Get the book with the given isbn
-    return jsonify(book) # Return the book as a json response
-
+    Admin_User(None).removeBook(isbn) # Remove the book from the inventory
+    return redirect(url_for('index')) # Redirect to the index page
 
 @app.route('/borrow_book/<isbn>') # Request parameter : isbn
 def borrow_book(isbn): # Function to borrow a book
-    if borrowBook(isbn): # If the book is borrowed successfully
-        return redirect(url_for('index')) # Redirect to the index page
-    return redirect(url_for('index')) # Redirect to the index page
+    if User(None).borrowBook(isbn): # If the book is borrowed successfully
+        return redirect(url_for('index_user')) # Redirect to the index page
+    return redirect(url_for('index_user')) # Redirect to the index page
 
 @app.route('/return_book/<isbn>') # Request parameter : isbn
 def return_book(isbn): # Function to return a borrowed book
-    if returnBorrowedBook(isbn): # If the book is returned successfully
-        return redirect(url_for('index')) # Redirect to the index page
-    return redirect(url_for('index')) 
+    if User.returnBorrowedBook(isbn): # If the book is returned successfully
+        return redirect(url_for('index_user')) # Redirect to the index page
+    flash("An Unexpected Error Occured D:")
+
+users_db = library().getUsers()
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username in users_db and users_db[username]['password'] == password:
+            role = users_db[username]['role']
+            if role == "Admin":
+                user = Admin_User(username)
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                user = User(username)
+                login_user(user)
+                return redirect(url_for("index_user"))
+
+            
+        flash('Invalid credentials')
+    return render_template('login.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])   
+def logout():
+    logout_user()
+    session.clear()  # Optional: clear the session
+    return redirect(url_for('login'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # newest_user_id = users_db[list(users_db.keys())[-1]]
+        if username not in users_db.keys():
+            users_db[username] = {"password": password, "role": "User", "id":200}
+            library().saveUser(users_db)
+            flash('Signup successful! You can now log in.')
+            return redirect(url_for('login'))
+        flash('Username already exists')
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
