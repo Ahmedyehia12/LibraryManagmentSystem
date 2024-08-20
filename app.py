c2580a69ecb578a26123d5da8d71d333d095ea9f
Ahmed yehia <97615630+Ahmedyehia12@users.noreply.github.com>
2024-08-20 12:58:06 +0300

    adding features

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, flash, session
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-#hi there
 import json
 
 app = Flask(__name__)
