06de45d9fac7445087ea32b4c1495ead1f6c73aa
Marwan Mohammed Sayed <155132957+MarwanMohammed2500@users.noreply.github.com>
2024-09-05 12:35:24 +0300

    Add files via upload

---
+++ Terraform/main_creation/modules/nat-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "subnet_id" {
+  description = "The ID of the subnet for the NAT Gateway"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the NAT Gateway and EIP"
+  type        = string
+}
