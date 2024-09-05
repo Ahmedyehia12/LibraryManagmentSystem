84dd8d2a65ba1b76db0eb473f226263e5086d3a2
Marwan Mohammed Sayed <155132957+MarwanMohammed2500@users.noreply.github.com>
2024-09-05 12:40:56 +0300

    Add files via upload

---
+++ Terraform/main_creation/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the VPC"
+  type        = string
+}
