f2f2693f7d42d4e9a1df3d6de14ed2c5f444a532
Marwan Mohammed Sayed <155132957+MarwanMohammed2500@users.noreply.github.com>
2024-09-05 12:34:21 +0300

    Add files via upload

---
+++ Terraform/main_creation/modules/internet-gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "The VPC ID to attach the internet gateway to"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the internet gateway"
+  type        = string
+}
